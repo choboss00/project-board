4d90a271b2d531f696cf97524577a44ef18d736f
Jin_00 <anjdal64@gmail.com>
2024-01-19 22:29:00 +0900

    #10 - 테스트 데이터 data.sql 추가
    테스트 생성하는 mockaroo 사용
    * https://www.mockaroo.com/

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO article (title, content, hashtag, created_by, modified_by, created_at, modified_at)
+VALUES
+    ('Pellentesque ultrices mattis odio.', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.', NULL, 'Mohandas', 'Annecorinne', '2023-08-27', '2024-01-11'),
+    ('Maecenas ut massa quis augue luctus tincidunt.', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 'Fuscia', 'Lydon', 'Aldo', '2023-03-15', '2023-12-11'),
+    ('Cras in purus eu magna vulputate luctus.', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', NULL, 'Vaughn', 'Karla', '2023-08-14', '2023-09-04'),
+    ('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'Fusce consequat. Nulla nisl. Nunc nisl.', 'Puce', 'Alikee', 'Noelyn', '2023-04-14', '2023-12-21'),
+    ('Etiam vel augue.', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', NULL, 'Eugene', 'Ardelis', '2023-02-09', '2024-01-30'),
+    ('Nulla ac enim.', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 'Fuscia', 'Eartha', 'Anselm', '2023-12-24', '2024-03-24'),
+    ('Donec semper sapien a libero.', 'Fusce consequat. Nulla nisl. Nunc nisl.', 'Crimson', 'Val', 'Billi', '2024-01-02', '2023-11-05'),
+    ('In sagittis dui vel nisl.', 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 'Green', 'Gregor', 'Tabby', '2023-09-04', '2023-12-16');
