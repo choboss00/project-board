80ec806b2aa5c850c2ca97c5b9490175ae9c3107
Jin_00 <anjdal64@gmail.com>
2024-01-21 20:31:34 +0900

    #1 - Querydsl 지원을 활용한 API 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer` 를 이용

--- src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
@@ -1,9 +1,29 @@
 package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.Article;
+import com.fastcampus.fastcampusprojectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // Article 엔티티 안에 있는 모든 필드에 대한 검색 기능 추가
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true); // 검색 대상에서 제외
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy); // 검색 대상에 포함
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // 제목에 대한 검색
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // 제목에 대한 검색
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // 제목에 대한 검색
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // 제목에 대한 검색
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // 제목에 대한 검색
+
+    }
 }
